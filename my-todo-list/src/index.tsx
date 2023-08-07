266e3f2a4ffa93bfb43441c3085806ccdf995fe1
한승준 <tmdwns950428@naver.com>
2023-08-07 15:46:04 +0900

    env: react-query setting

--- my-todo-list/src/index.tsx
+++ my-todo-list/src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { QueryClientProvider, QueryClient } from "react-query";
 
 if (process.env.NODE_ENV === "development") {
   const { worker } = require("./mock/browser");
@@ -12,13 +12,16 @@ if (process.env.NODE_ENV === "development") {
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 성능 측정
+reportWebVitals(console.log);
